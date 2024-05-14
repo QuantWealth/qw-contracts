e996e90b302ea6b8341b4a67a99b6791f8018e62
sanchaymittal <sanchaymittal@gmail.com>
2024-05-15 03:17:32 +0400

    chore: lint fixes

--- test/unit/QWManager.t.sol
+++ test/unit/QWManager.t.sol
@@ -8,50 +8,50 @@ import {MockQWRegistry} from 'test/smock/MockQWRegistry.sol';
 import {MockQWAave} from 'test/smock/child/MockQWAave.sol';
 
 contract QWManagerTest is Test {
-    MockQWManager public mockQWManager;
-    // MockQWRegistry public mockQWRegistry;
-    MockQWAave public mockQWAave;
-    address[] public targetQWChild;
-    address public tokenAddress;
-    uint256 public amount;
+  MockQWManager public mockQWManager;
+  // MockQWRegistry public mockQWRegistry;
+  MockQWAave public mockQWAave;
+  address[] public targetQWChild;
+  address public tokenAddress;
+  uint256 public amount;
 
-    function setUp() public {
-        mockQWManager = new MockQWManager();
-        // mockQWRegistry = new MockQWRegistry(mockQWManager.REGISTRY());
-        mockQWAave = new MockQWAave(address(mockQWManager), address(0x456));
-        targetQWChild = [address(mockQWAave)];
-        
-        tokenAddress = address(0x123);
-        amount = 100;
-    }
+  function setUp() public {
+    mockQWManager = new MockQWManager();
+    // mockQWRegistry = new MockQWRegistry(mockQWManager.REGISTRY());
+    mockQWAave = new MockQWAave(address(mockQWManager), address(0x456));
+    targetQWChild = [address(mockQWAave)];
 
-    function test_Execute_Success() public {
-        bytes[] memory callData = new bytes[](1);
-        // Mock a successful execution
-        mockQWManager.mock_call_execute(targetQWChild, callData, tokenAddress, amount);
+    tokenAddress = address(0x123);
+    amount = 100;
+  }
 
-        // Call the execute function
-        mockQWManager.execute(targetQWChild, callData, tokenAddress, amount);
-    }
+  function test_Execute_Success() public {
+    bytes[] memory callData = new bytes[](1);
+    // Mock a successful execution
+    mockQWManager.mock_call_execute(targetQWChild, callData, tokenAddress, amount);
 
-    function test_Close_Success() public {
-        bytes[] memory callData = new bytes[](1);
-        callData[0] = abi.encode(address(0x123), uint256(100));
-        // Mock a successful closing
-        mockQWManager.mock_call_close(targetQWChild, callData);
+    // Call the execute function
+    mockQWManager.execute(targetQWChild, callData, tokenAddress, amount);
+  }
 
-        // Call the close function
-        mockQWManager.close(targetQWChild, callData);
-    }
+  function test_Close_Success() public {
+    bytes[] memory callData = new bytes[](1);
+    callData[0] = abi.encode(address(0x123), uint256(100));
+    // Mock a successful closing
+    mockQWManager.mock_call_close(targetQWChild, callData);
 
-    function test_Withdraw_Success() public {
-        address user = address(0x789);
-        uint256 withdrawAmount = 50;
+    // Call the close function
+    mockQWManager.close(targetQWChild, callData);
+  }
 
-        // Mock a successful withdrawal
-        mockQWManager.mock_call_withdraw(user, withdrawAmount);
+  function test_Withdraw_Success() public {
+    address user = address(0x789);
+    uint256 withdrawAmount = 50;
 
-        // Call the withdraw function
-        mockQWManager.withdraw(user, withdrawAmount);
-    }
+    // Mock a successful withdrawal
+    mockQWManager.mock_call_withdraw(user, withdrawAmount);
+
+    // Call the withdraw function
+    mockQWManager.withdraw(user, withdrawAmount);
+  }
 }
--- test/unit/child/QWAave.t.sol
+++ test/unit/child/QWAave.t.sol
@@ -6,39 +6,39 @@ import {Test} from 'forge-std/Test.sol';
 import {MockQWAave} from 'test/smock/child/MockQWAave.sol';
 
 contract QWAaveTest is Test {
-    MockQWAave public mockQWAave;
-    address public qwManager;
-    address public pool;
+  MockQWAave public mockQWAave;
+  address public qwManager;
+  address public pool;
 
-    function setUp() public {
-        qwManager = address(0x123);
-        pool = address(0x456);
-        mockQWAave = new MockQWAave(qwManager, pool);
-    }
+  function setUp() public {
+    qwManager = address(0x123);
+    pool = address(0x456);
+    mockQWAave = new MockQWAave(qwManager, pool);
+  }
 
-    function test_Create_Success() public {
-        bytes memory callData = '';
-        address tokenAddress = address(0x789);
-        uint256 amount = 100;
+  function test_Create_Success() public {
+    bytes memory callData = '';
+    address tokenAddress = address(0x789);
+    uint256 amount = 100;
 
-        // Mock a successful call to IPool.supply
-        mockQWAave.mock_call_create(callData, tokenAddress, amount, true);
+    // Mock a successful call to IPool.supply
+    mockQWAave.mock_call_create(callData, tokenAddress, amount, true);
 
-        // Call the create function
-        bool success = mockQWAave.create(callData, tokenAddress, amount);
+    // Call the create function
+    bool success = mockQWAave.create(callData, tokenAddress, amount);
 
-        assertTrue(success, "Create function should return true on success");
-    }
+    assertTrue(success, 'Create function should return true on success');
+  }
 
-    function test_Close_Success() public {
-        bytes memory callData = abi.encode(address(0x123), uint256(100));
+  function test_Close_Success() public {
+    bytes memory callData = abi.encode(address(0x123), uint256(100));
 
-        // Mock a successful call to IPool.withdraw
-        mockQWAave.mock_call_close(callData, true);
+    // Mock a successful call to IPool.withdraw
+    mockQWAave.mock_call_close(callData, true);
 
-        // Call the close function
-        bool success = mockQWAave.close(callData);
+    // Call the close function
+    bool success = mockQWAave.close(callData);
 
-        assertTrue(success, "Close function should return true on success");
-    }
+    assertTrue(success, 'Close function should return true on success');
+  }
 }
