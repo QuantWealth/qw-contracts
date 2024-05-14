3c6389d51b9b212f2b19a6f2d3641af2523f910e
sanchaymittal <sanchaymittal@gmail.com>
2024-05-15 03:16:53 +0400

    feat: qwregistry unit test WIP

---
+++ test/unit/QWRegistry.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import {MockQWRegistry} from 'test/smock/MockQWRegistry.sol';
+import {Test} from 'forge-std/Test.sol';
+
+contract QWRegistryTest is Test {
+    MockQWRegistry mockQWRegistry;
+    address constant validQWManager = address(0x123);
+    address constant validChildContract = address(0x456);
+    address constant invalidChildContract = address(0x789);
+
+    MockQWRegistry mockRegistry;
+
+    function beforeEach() public {
+        mockQWRegistry = new MockQWRegistry(validQWManager);
+    }
+
+    function testRegisterChild() public {
+        // mockQWRegistry.mock_call_registerChild(validChildContract);
+        // mockQWRegistry.registerChild(validChildContract);
+        // assertTrue(mockQWRegistry.whitelist(validChildContract), "Child contract should be whitelisted after registration");
+    }
+
+    // function testRegisterInvalidChild() public {
+    //     mockRegistry.set_whitelist(invalidChildContract, false);
+    //     try mockQWRegistry.registerChild(invalidChildContract) {
+    //         fail("Registering an invalid child contract should revert");
+    //     } catch Error(string memory reason) {
+    //         assertEq(reason, "ParentMismatch", "Error message should be 'ParentMismatch'");
+    //     } catch (bytes memory) {
+    //         fail("Registering an invalid child contract should revert with reason 'ParentMismatch'");
+    //     }
+    // }
+}
