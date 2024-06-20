380976aa7ac3cb0fc03429aeb0bac1ccebc13a76
Jake Kidd <jcb.kdd@gmail.com>
2024-06-20 11:29:55 -0400

    refactor: shares functionality

--- src/contracts/QWManager.sol
+++ src/contracts/QWManager.sol
@@ -4,115 +4,128 @@ pragma solidity 0.8.23;
 import {QWRegistry} from './QWRegistry.sol';
 import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
 import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-
 import {IQWChild} from 'interfaces/IQWChild.sol';
 import {IQWManager} from 'interfaces/IQWManager.sol';
 import {IQWRegistry} from 'interfaces/IQWRegistry.sol';
+import {QWShares} from './QWShares.sol';
 
 /**
  * @title Quant Wealth Manager Contract
  * @notice This contract manages the execution, closing, and withdrawal of various strategies for Quant Wealth.
  */
-contract QWManager is IQWManager, Ownable {
-  // Variables
-  address public immutable REGISTRY;
+contract QWManager is IQWManager, Ownable, QWShares {
+    // Variables
+    address public immutable REGISTRY;
 
-  // Custom errors
-  error InvalidInputLength(); // Error for mismatched input lengths
-  error ContractNotWhitelisted(); // Error for contract not whitelisted
-  error CallFailed(); // Error for call failed
+    // Custom errors
+    error InvalidInputLength(); // Error for mismatched input lengths
+    error ContractNotWhitelisted(); // Error for contract not whitelisted
+    error CallFailed(); // Error for call failed
 
-  // Constructor
-  constructor() Ownable(msg.sender) {
-    // Deploy the QWRegistry contract and set the REGISTRY address
-    QWRegistry _registry = new QWRegistry(address(this), msg.sender);
-    REGISTRY = address(_registry);
-  }
-
-  // External Functions
-
-  /**
-   * @notice Execute a series of investments.
-   * Transfers specified amounts of tokens and calls target contracts with provided calldata.
-   * @param _targetQwChild List of contract addresses to interact with.
-   * @param _callData Encoded function calls to be executed on the target contracts.
-   * @param _tokenAddress Token address to transfer.
-   * @param _amount Amount of tokens to transfer to each target contract.
-   */
-  function execute(
-    address[] memory _targetQwChild,
-    bytes[] memory _callData,
-    address _tokenAddress,
-    uint256 _amount
-  ) external onlyOwner {
-    if (_targetQwChild.length != _callData.length) {
-      revert InvalidInputLength();
+    // Constructor
+    constructor() Ownable(msg.sender) {
+        // Deploy the QWRegistry contract and set the REGISTRY address
+        QWRegistry _registry = new QWRegistry(address(this), msg.sender);
+        REGISTRY = address(_registry);
     }
 
-    for (uint256 i = 0; i < _targetQwChild.length; i++) {
-      // Check if the target contract is whitelisted
-      if (!IQWRegistry(REGISTRY).whitelist(_targetQwChild[i])) {
-        revert ContractNotWhitelisted();
-      }
+    // External Functions
 
-      // Approve the target contract to spend the specified amount of tokens
-      IERC20 token = IERC20(_tokenAddress);
-      token.approve(address(_targetQwChild[i]), _amount);
+    /**
+     * @notice Execute a series of investments.
+     * Transfers specified amounts of tokens and calls target contracts with provided calldata.
+     * @param _targetQwChild List of contract addresses to interact with.
+     * @param _callData Encoded function calls to be executed on the target contracts.
+     * @param _tokenAddress Token address to transfer.
+     * @param _amount Amount of tokens to transfer to each target contract.
+     */
+    function execute(
+        address[] memory _targetQwChild,
+        bytes[] memory _callData,
+        address _tokenAddress,
+        uint256 _amount
+    ) external onlyOwner {
+        if (_targetQwChild.length != _callData.length) {
+            revert InvalidInputLength();
+        }
 
-      // Call the create function on the target contract with the provided calldata
-      (bool success) = IQWChild(_targetQwChild[i]).create(_callData[i], _tokenAddress, _amount);
-      if (!success) {
-        revert CallFailed();
-      }
+        for (uint256 i = 0; i < _targetQwChild.length; i++) {
+            // Check if the target contract is whitelisted
+            if (!IQWRegistry(REGISTRY).whitelist(_targetQwChild[i])) {
+                revert ContractNotWhitelisted();
+            }
+
+            // Approve the target contract to spend the specified amount of tokens
+            IERC20 token = IERC20(_tokenAddress);
+            token.approve(address(_targetQwChild[i]), _amount);
+
+            // Encode necessary data for child contract
+            bytes memory encodedData = abi.encode(totalShares[_targetQwChild[i]]);
+
+            // Call the create function on the target contract with the provided calldata
+            (bool success, bytes memory result) = IQWChild(_targetQwChild[i]).create(encodedData, _tokenAddress, _amount);
+            if (!success) {
+                revert CallFailed();
+            }
+
+            // Decode shares from result
+            (uint256 shares) = abi.decode(result, (uint256));
+
+            // Update shares in QWManager
+            _updateSharesOnDeposit(msg.sender, shares, _targetQwChild[i]);
+        }
     }
-  }
 
-  /**
-   * @notice Close a series of investments.
-   * Calls target contracts with provided calldata to close positions.
-   * @param _targetQwChild List of contract addresses to interact with.
-   * @param _callData Encoded function calls to be executed on the target contracts.
-   */
-  function close(address[] memory _targetQwChild, bytes[] memory _callData) external onlyOwner {
-    if (_targetQwChild.length != _callData.length) {
-      revert InvalidInputLength();
+    /**
+     * @notice Close a series of investments.
+     * Calls target contracts with provided calldata to close positions.
+     * @param _targetQwChild List of contract addresses to interact with.
+     * @param _callData Encoded function calls to be executed on the target contracts.
+     */
+    function close(address[] memory _targetQwChild, bytes[] memory _callData) external onlyOwner {
+        if (_targetQwChild.length != _callData.length) {
+            revert InvalidInputLength();
+        }
+
+        for (uint256 i = 0; i < _targetQwChild.length; i++) {
+            // Decode the calldata to get the LP asset address and amount
+            (address _user, uint256 _sharesAmount) = abi.decode(_callData[i], (address, uint256));
+
+            // Encode necessary data for child contract
+            bytes memory encodedData = abi.encode(_user, _sharesAmount, totalShares[_targetQwChild[i]], _targetQwChild[i]);
+
+            // Call the close function on the target contract with the provided calldata
+            (bool success) = IQWChild(_targetQwChild[i]).close(encodedData);
+            if (!success) {
+                revert CallFailed();
+            }
+
+            // Update shares in QWManager
+            _updateSharesOnWithdrawal(_user, _sharesAmount, _targetQwChild[i]);
+        }
     }
 
-    for (uint256 i = 0; i < _targetQwChild.length; i++) {
-      // Decode the calldata to get the LP asset address and amount
-      (, address lpAsset, uint256 amount) = abi.decode(_callData[i], (address, address, uint256));
-      IERC20 token = IERC20(lpAsset);
-      token.approve(address(_targetQwChild[i]), amount);
-
-      // Call the close function on the target contract with the provided calldata
-      (bool success) = IQWChild(_targetQwChild[i]).close(_callData[i]);
-      if (!success) {
-        revert CallFailed();
-      }
+    /**
+     * @notice Withdraw funds to a specified user.
+     * Transfers a specified amount of funds to the user.
+     * @param user The address of the user to receive the funds.
+     * @param _tokenAddress The address of the token to transfer.
+     * @param _amount The amount of funds to transfer to the user.
+     */
+    function withdraw(address user, address _tokenAddress, uint256 _amount) external onlyOwner {
+        IERC20 token = IERC20(_tokenAddress);
+        token.transfer(user, _amount);
     }
-  }
 
-  /**
-   * @notice Withdraw funds to a specified user.
-   * Transfers a specified amount of funds to the user.
-   * @param user The address of the user to receive the funds.
-   * @param _tokenAddress The address of the token to transfer.
-   * @param _amount The amount of funds to transfer to the user.
-   */
-  function withdraw(address user, address _tokenAddress, uint256 _amount) external onlyOwner {
-    IERC20 token = IERC20(_tokenAddress);
-    token.transfer(user, _amount);
-  }
-
-  /**
-   * @notice Receive funds from a specified user.
-   * Transfers a specified amount of funds from the user to this contract.
-   * @param user The address of the user sending the funds.
-   * @param _tokenAddress The address of the token to transfer.
-   * @param _amount The amount of funds to transfer to this contract.
-   */
-  function receiveFunds(address user, address _tokenAddress, uint256 _amount) external {
-    IERC20 token = IERC20(_tokenAddress);
-    token.transferFrom(user, address(this), _amount);
-  }
+    /**
+     * @notice Receive funds from a specified user.
+     * Transfers a specified amount of funds from the user to this contract.
+     * @param user The address of the user sending the funds.
+     * @param _tokenAddress The address of the token to transfer.
+     * @param _amount The amount of funds to transfer to this contract.
+     */
+    function receiveFunds(address user, address _tokenAddress, uint256 _amount) external {
+        IERC20 token = IERC20(_tokenAddress);
+        token.transferFrom(user, address(this), _amount);
+    }
 }
--- src/contracts/child/QWAaveV2.sol
+++ src/contracts/child/QWAaveV2.sol
@@ -10,75 +10,117 @@ import {ILendingPool} from 'interfaces/aave-v2/ILendingPool.sol';
  * @notice This contract integrates with AaveV2 protocol for Quant Wealth management.
  */
 contract QWAaveV2 is IQWChild {
-  // Variables
-  address public immutable QW_MANAGER;
-  address public immutable LENDING_POOL;
+    // Variables
+    address public immutable QW_MANAGER;
+    address public immutable LENDING_POOL;
 
-  // Custom errors
-  error InvalidCallData(); // Error for invalid call data
-  error UnauthorizedAccess(); // Error for unauthoruzed caller
+    // Custom errors
+    error InvalidCallData(); // Error for invalid call data
+    error UnauthorizedAccess(); // Error for unauthorized caller
 
-  modifier onlyQwManager() {
-    if (msg.sender != QW_MANAGER) {
-      revert UnauthorizedAccess();
-    }
-    _;
-  }
-
-  /**
-   * @dev Constructor to initialize the contract with required addresses.
-   * @param _qwManager The address of the Quant Wealth Manager contract.
-   * @param _lendingPool The address of the AaveV2 pool contract.
-   */
-  constructor(address _qwManager, address _lendingPool) {
-    QW_MANAGER = _qwManager;
-    LENDING_POOL = _lendingPool;
-  }
-
-  // Functions
-  /**
-   * @notice Executes a transaction on AaveV2 pool to deposit tokens.
-   * @dev This function is called by the parent contract to deposit tokens into the AaveV2 pool.
-   * @param _callData Encoded function call data (not used in this implementation).
-   * @param _tokenAddress Address of the token to be deposited.
-   * @param _amount Amount of tokens to be deposited.
-   * @return success boolean indicating the success of the transaction.
-   */
-  function create(
-    bytes memory _callData,
-    address _tokenAddress,
-    uint256 _amount
-  ) external override onlyQwManager returns (bool success) {
-    if (_callData.length != 0) {
-      revert InvalidCallData();
+    modifier onlyQwManager() {
+        if (msg.sender != QW_MANAGER) {
+            revert UnauthorizedAccess();
+        }
+        _;
     }
 
-    IERC20 token = IERC20(_tokenAddress);
-    token.transferFrom(QW_MANAGER, address(this), _amount);
-    token.approve(LENDING_POOL, _amount);
-
-    ILendingPool(LENDING_POOL).deposit(_tokenAddress, _amount, QW_MANAGER, 0);
-    return true;
-  }
-
-  /**
-   * @notice Executes a transaction on AaveV2 pool to withdraw tokens.
-   * @dev This function is called by the parent contract to withdraw tokens from the AaveV2 pool.
-   * @param _callData Encoded function call data containing the asset and amount to be withdrawn.
-   * @return success boolean indicating the success of the transaction.
-   */
-  function close(bytes memory _callData) external override onlyQwManager returns (bool success) {
-    if (_callData.length == 0) {
-      revert InvalidCallData();
+    /**
+     * @dev Constructor to initialize the contract with required addresses.
+     * @param _qwManager The address of the Quant Wealth Manager contract.
+     * @param _lendingPool The address of the AaveV2 pool contract.
+     */
+    constructor(address _qwManager, address _lendingPool) {
+        QW_MANAGER = _qwManager;
+        LENDING_POOL = _lendingPool;
     }
 
-    (address asset, address lpAsset, uint256 amount) = abi.decode(_callData, (address, address, uint256));
+    // Functions
+    /**
+     * @notice Executes a transaction on AaveV2 pool to deposit tokens.
+     * @dev This function is called by the parent contract to deposit tokens into the AaveV2 pool.
+     * @param _callData Encoded function call data containing user address and total shares.
+     * @param _tokenAddress Address of the token to be deposited.
+     * @param _amount Amount of tokens to be deposited.
+     * @return success boolean indicating the success of the transaction.
+     */
+    function create(
+        bytes memory _callData,
+        address _tokenAddress,
+        uint256 _amount
+    ) external override onlyQwManager returns (bool success) {
+        (uint256 _totalShares) = abi.decode(_callData, (uint256));
 
-    IERC20 token = IERC20(lpAsset);
-    token.transferFrom(QW_MANAGER, address(this), amount);
-    token.approve(LENDING_POOL, amount);
+        IERC20 token = IERC20(_tokenAddress);
+        token.transferFrom(QW_MANAGER, address(this), _amount);
+        token.approve(LENDING_POOL, _amount);
 
-    ILendingPool(LENDING_POOL).withdraw(asset, amount, QW_MANAGER);
-    return true;
-  }
+        ILendingPool(LENDING_POOL).deposit(_tokenAddress, _amount, address(this), 0);
+        uint256 aTokensReceived = IERC20(_tokenAddress).balanceOf(address(this));
+
+        uint256 shares;
+        if (_totalShares == 0) {
+            shares = aTokensReceived;
+        } else {
+            uint256 totalPoolValue = getTotalPoolValue(_tokenAddress);
+            shares = (aTokensReceived * _totalShares) / totalPoolValue;
+        }
+
+        success = true;
+
+        // Encode shares to return back to QWManager
+        bytes memory returnData = abi.encode(success, shares);
+        assembly {
+            return(add(returnData, 32), mload(returnData))
+        }
+    }
+
+    /**
+     * @notice Executes a transaction on AaveV2 pool to withdraw tokens.
+     * @dev This function is called by the parent contract to withdraw tokens from the AaveV2 pool.
+     * @param _callData Encoded function call data containing user address, shares amount, and total shares.
+     * @return success boolean indicating the success of the transaction.
+     */
+    function close(
+        bytes memory _callData
+    ) external override onlyQwManager returns (bool success) {
+        (address _user, uint256 _sharesAmount, uint256 _totalShares, address _tokenAddress) = abi.decode(_callData, (address, uint256, uint256, address));
+
+        if (_sharesAmount > _totalShares) {
+            revert InvalidCallData();
+        }
+
+        uint256 totalSharesValue = getTotalPoolValue(_tokenAddress);
+        uint256 amountToWithdraw = (_sharesAmount * totalSharesValue) / _totalShares;
+
+        ILendingPool(LENDING_POOL).withdraw(_tokenAddress, amountToWithdraw, QW_MANAGER);
+        success = true;
+
+        // Encode success to return back to QWManager
+        bytes memory returnData = abi.encode(success);
+        assembly {
+            return(add(returnData, 32), mload(returnData))
+        }
+    }
+
+    /**
+     * @notice Gets the price per share in terms of the specified token.
+     * @dev This function calculates the value of one share in terms of the specified token.
+     * @param _tokenAddress The address of the token to get the price per share in.
+     * @return pricePerShare uint256 representing the value of one share in the specified token.
+     */
+    function pricePerShare(address _tokenAddress) external view returns (uint256) {
+        uint256 totalSharesValue = getTotalPoolValue(_tokenAddress);
+        return totalSharesValue / IERC20(_tokenAddress).totalSupply();
+    }
+
+    /**
+     * @notice Gets the total pool value in terms of the specified token.
+     * @dev This function calculates the total value of the pool in terms of the specified token.
+     * @param _tokenAddress The address of the token to get the total pool value in.
+     * @return poolValue uint256 representing the total value of the pool in the specified token.
+     */
+    function getTotalPoolValue(address _tokenAddress) public view returns (uint256 poolValue) {
+        return IERC20(_tokenAddress).balanceOf(address(this));
+    }
 }
